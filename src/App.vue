a0c336773b8dcc2f9e95805edb434d6794215ae1
nowakrafal9 <nowakr99@gmail.com>
2024-06-09 12:13:00 +0200

    Moved and renamed header, added footer. More CSS adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
     <Header />
     <main>
       <router-view></router-view>
     </main>
-  </div>
 </template>
 
 <script>
-import Header from './components/StyleHeader.vue';
+import Header from './components/html-fragments/HeaderFragment.vue';
 
 export default {
   components: {
--- src/components/html-fragments/HeaderFragment.vue
+++ src/components/html-fragments/HeaderFragment.vue
@@ -1,16 +1,17 @@
 <template>
-    <header>
+    <header class="no-select">
         <router-link to="/"><img src="@/assets/logo.svg" alt="Ciastkarnia Logo" class="logo"></router-link>
         <nav>
-            <div class="user-info" v-if="!user">
-                Użytkownik:<br/> niezalogowany <br/>
+            <div class="user-info no-select" v-if="!user">
+                <span class="login-status">Niezalogowano</span>
                 <router-link to="/login" v-if="!user" class="login-button">Zaloguj się</router-link>
             </div>
+
             <div class="user-info" v-if="user">
-                <span @click="toggleDropdown">
-                    Użytkownik:<br/>
+                <span @click="toggleDropdown" class="login-status">
+                    <img src="@/assets/user.svg" alt="User img" class="user-logo">
                     {{ userDisplayName }} 
-                    <span class="triangle">&#9660;</span>
+                    <span class="triangle" :class="{ open: isDropdownOpen }">&#9660;</span>
                 </span>
                 <div v-if="isDropdownOpen" class="dropdown-content" ref="dropdownContent">
                     <router-link v-if="userRole === 'employee' || userRole === 'admin'" to="/zamowienia-pracownik">Podgląd zamówień</router-link>
@@ -20,7 +21,7 @@
 
                     <router-link v-if="userRole === 'user' || userRole === 'admin'" to="/koszyk">Koszyk</router-link>
                     <router-link to="/moje-zamowienia">Moje zamówienia</router-link>
-                    <button @click="logout">Wyloguj</button>
+                    <button @click="logout" class="logout">Wyloguj</button>
                 </div>
             </div>
         </nav>
@@ -119,37 +120,93 @@ nav a:hover {
 }
 
 .user-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     position: relative;
+}
+
+.user-info .login-status {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     cursor: pointer;
+    font-weight: bolder;
+    font-size: 1rem;
+    color: #7f3f00;
+}
+
+.user-info .login-button {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #7f3f00;
+    color: #E7A66C;
+    border: 2px solid #7f3f00;
+    border-radius: 5px;
+    text-decoration: none;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+    transition: background-color 0.2s, color 0.2s;
+}
+
+.user-info .login-button:hover {
+    background-color: #E7A66C;
+    color: #7f3f00;
+    font-size: 1rem;
+}
+
+.user-info .user-logo {
+    width: 40px;
+    height: 40px;
+    margin-bottom: 5px;
 }
 
 .user-info .triangle {
-    display: inline-block;
+    display: block;
+    text-align: center;
+    color: #7f3f00;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%) rotate(90deg);
+    transition: transform 0.3s;
+}
+
+.user-info .triangle.open {
+    transform: translateY(-50%) rotate(0deg);
 }
 
 .user-info .dropdown-content {
-    display: none;
     position: absolute;
-    top: 30px;
+    top: 100%;
     right: 0;
-    background-color: white;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    background-color: #ffffff;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
     z-index: 1;
-    color: black;
-    padding: 1rem;
-    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+    box-sizing: border-box;
 }
 
+.user-info .dropdown-content .logout{
+    background-color: #E7A66C;
+    font-weight: bold;
+}
 .user-info .dropdown-content a,
 .user-info .dropdown-content button {
-    display: block;
-    padding: 0.5rem 0;
+    color: #7f3f00;
+    padding: 12px 16px;
     text-decoration: none;
-    color: black;
-    border: none;
-    background: none;
-    cursor: pointer;
-    text-align: left;
+    font-weight: bold;
+    display: block;
+    text-align: center;
+    width: 100%;
+    box-sizing: border-box;
 }
 
 .user-info .dropdown-content a:hover,
@@ -157,7 +214,11 @@ nav a:hover {
     background-color: #f1f1f1;
 }
 
-.user-info .dropdown-content {
-    display: block;
+.no-select {
+    user-select: none; /* Standard CSS3 */
+    -webkit-user-select: none; /* Safari */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
 }
+
 </style>
