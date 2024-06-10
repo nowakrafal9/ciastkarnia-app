bb962670ba89e75b871c0f7baa98ac7e3395475f
nowakrafal9 <nowakr99@gmail.com>
2024-06-10 20:22:59 +0200

    Improved CSS for user list

--- src/components/admin/AdminUsersList.vue
+++ src/components/admin/AdminUsersList.vue
@@ -1,40 +1,47 @@
 <template>
-  <div>
-    <h1>Zarządzanie Kontami Użytkowników</h1>
-    <div class="filters">
-      <div>
-        <label for="emailFilter">Email:</label>
-        <input type="text" v-model="filters.email" @input="applyFilters">
-      </div>
-      <div>
-        <label for="firstNameFilter">Imię:</label>
-        <input type="text" v-model="filters.firstName" @input="applyFilters">
-      </div>
-      <div>
-        <label for="lastNameFilter">Nazwisko:</label>
-        <input type="text" v-model="filters.lastName" @input="applyFilters">
-      </div>
-      <div>
-        <label for="phoneNumberFilter">Numer Telefonu:</label>
-        <input type="text" v-model="filters.phoneNumber" @input="applyFilters">
-      </div>
-      <div>
-        <label>Role:</label>
-        <div>
-          <input type="checkbox" id="userRole" value="user" v-model="filters.roles" @change="applyFilters">
-          <label for="userRole">User</label>
-        </div>
-        <div>
-          <input type="checkbox" id="employeeRole" value="employee" v-model="filters.roles" @change="applyFilters">
-          <label for="employeeRole">Employee</label>
-        </div>
-        <div>
-          <input type="checkbox" id="adminRole" value="admin" v-model="filters.roles" @change="applyFilters">
-          <label for="adminRole">Admin</label>
-        </div>
-      </div>
+  <div class="admin-panel">
+    <h1>Panel Administratora - zarządzanie kontami</h1>
+    <button @click="toggleFilters" class="btn-toggle-filters">
+      {{ showFilters ? 'Ukryj Filtry' : 'Pokaż Filtry' }}
+    </button>
+    <div v-if="showFilters" class="filters">
+      <table>
+        <tr>
+          <td><label for="emailFilter">Email:</label></td>
+          <td><input type="text" v-model="filters.email" @input="applyFilters"></td>
+        </tr>
+        <tr>
+          <td><label for="firstNameFilter">Imię:</label></td>
+          <td><input type="text" v-model="filters.firstName" @input="applyFilters"></td>
+        </tr>
+        <tr>
+          <td><label for="lastNameFilter">Nazwisko:</label></td>
+          <td><input type="text" v-model="filters.lastName" @input="applyFilters"></td>
+        </tr>
+        <tr>
+          <td><label for="phoneNumberFilter">Numer Telefonu:</label></td>
+          <td><input type="text" v-model="filters.phoneNumber" @input="applyFilters"></td>
+        </tr>
+        <tr>
+          <td><label>Role:</label></td>
+          <td>
+            <div>
+              <input type="checkbox" id="userRole" value="user" v-model="filters.roles" @change="applyFilters">
+              <label for="userRole">User</label>
+            </div>
+            <div>
+              <input type="checkbox" id="employeeRole" value="employee" v-model="filters.roles" @change="applyFilters">
+              <label for="employeeRole">Employee</label>
+            </div>
+            <div>
+              <input type="checkbox" id="adminRole" value="admin" v-model="filters.roles" @change="applyFilters">
+              <label for="adminRole">Admin</label>
+            </div>
+          </td>
+        </tr>
+      </table>
     </div>
-    <table>
+    <table class="user-table">
       <thead>
         <tr>
           <th>Email</th>
@@ -53,13 +60,15 @@
           <td>{{ user.phoneNumber || 'brak informacji' }}</td>
           <td>{{ user.role || 'brak informacji' }}</td>
           <td>
-            <button v-if="user.role === 'employee'" @click="deleteUser(user.id)">Usuń</button>
+            <button v-if="user.role === 'employee'" @click="confirmDeleteUser(user.id)"
+              class="btn btn-delete">Usuń</button>
           </td>
         </tr>
       </tbody>
     </table>
     <div class="pagination">
       <button @click="prevPage" :disabled="currentPage === 1">Poprzednia</button>
+      <span>Strona {{ currentPage }} z {{ totalPages }}</span>
       <button @click="nextPage" :disabled="currentPage === totalPages">Następna</button>
     </div>
   </div>
@@ -75,6 +84,7 @@ export default {
       users: [],
       currentPage: 1,
       perPage: 10,
+      showFilters: false,
       filters: {
         email: '',
         firstName: '',
@@ -130,8 +140,16 @@ export default {
       await deleteDoc(doc(db, 'users', userId));
       await this.fetchUsers();
     },
+    confirmDeleteUser(userId) {
+      if (confirm('Czy na pewno chcesz usunąć tego użytkownika?')) {
+        this.deleteUser(userId);
+      }
+    },
     applyFilters() {
       this.currentPage = 1; // Reset current page to 1 when filters are applied
+    },
+    toggleFilters() {
+      this.showFilters = !this.showFilters;
     },
     nextPage() {
       if (this.currentPage < this.totalPages) {
@@ -148,25 +166,107 @@ export default {
 </script>
 
 <style scoped>
+.admin-panel {
+  padding: 0 20px;
+}
+
+h1 {
+  text-align: center;
+  color: #7f3f00;
+}
+
+.btn-toggle-filters {
+  background-color: #E7A66C;
+  color: #7f3f00;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+  margin-bottom: 20px;
+}
+
+.btn-toggle-filters:hover {
+  background-color: #d9985c;
+}
+
 .filters {
   display: flex;
   flex-direction: column;
   margin-bottom: 20px;
 }
-.filters div {
-  margin-bottom: 10px;
+
+.filters table {
+  width: 25%;
 }
+
+.filters td {
+  padding: 5px;
+  vertical-align: middle;
+}
+
+.filters td,
+.filters th {
+  border: none;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border-bottom: 1px solid #7f3f00;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #E7A66C;
+  color: #7f3f00;
+}
+
+td .btn {
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+
+.btn-delete {
+  background-color: #f44336;
+  color: white;
+}
+
+.btn-delete:hover {
+  background-color: #da190b;
+}
+
 .pagination {
   display: flex;
   justify-content: space-between;
   margin-top: 20px;
 }
-table {
-  width: 100%;
-  border-collapse: collapse;
+
+.pagination button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
 }
-th, td {
-  border: 1px solid #ddd;
-  padding: 8px;
+
+.pagination button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.pagination button:not(:disabled):hover {
+  background-color: #d9985c;
 }
 </style>
